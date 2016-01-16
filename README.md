782bde55f79e2ff618e76901826e1267a0db73e8
kubo39 <kubo39@gmail.com>
2016-01-16 17:34:38 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ipc
+
+ipc is an implementation of file descriptor passing over Unix sockets on Linux.
---
+++ source/ipc.d
@@ -0,0 +1,138 @@
+module ipc;
+
+version(linux)
+{
+  private import core.sys.posix.sys.socket;
+  private import core.stdc.string : memcpy;
+
+  import std.socket : Socket,
+    socket_t,
+    SocketFlags,
+    AddressFamily,
+    SocketType,
+    ProtocolType,
+    SocketOSException;
+
+  class UnixSocket : Socket
+  {
+    this(SocketType socketType)
+    {
+      super(AddressFamily.UNIX, socketType, ProtocolType.IP);
+    }
+
+    this(socket_t sock, AddressFamily af)
+    {
+      super(sock, af);
+    }
+
+    ptrdiff_t _send(const(void)[] buf, SocketFlags flags) @trusted
+    {
+      msghdr msg;
+      iovec iov;
+      cmsghdr cmsg;
+
+      iov.iov_base = cast(void*)buf;
+      iov.iov_len = buf.length;
+
+      msg.msg_name = null;
+      msg.msg_namelen = 0;
+      msg.msg_control = &cmsg;
+      msg.msg_controllen = cast(socklen_t)CMSG_LEN(int.sizeof);
+      msg.msg_iov = &iov;
+      msg.msg_iovlen = 1;
+      msg.msg_flags = 0;
+
+      cmsg.cmsg_level = SOL_SOCKET;
+      cmsg.cmsg_type = SCM_RIGHTS;
+      cmsg.cmsg_len = CMSG_LEN(int.sizeof);
+
+      socket_t fd = this.handle(); /* access to sock. */
+      memcpy(CMSG_DATA(&cmsg), cast(void*)&fd, int.sizeof);
+
+      return sendmsg(fd, &msg, 0);
+    }
+
+    override ptrdiff_t send(const(void)[] buf, SocketFlags flags)
+    {
+      return _send(buf, flags);
+    }
+
+    override ptrdiff_t send(const(void)[] buf)
+    {
+      return send(buf, SocketFlags.NONE);
+    }
+
+    ptrdiff_t _receive(void[] buf, SocketFlags flags) @trusted
+    {
+      msghdr msg;
+      iovec iov;
+      cmsghdr cmsg;
+
+      iov.iov_base = cast(void*)buf;
+      iov.iov_len = buf.length;
+
+      msg.msg_name = null;
+      msg.msg_namelen = 0;
+      msg.msg_control = &cmsg;
+      msg.msg_controllen = cast(socklen_t)CMSG_LEN(int.sizeof);
+      msg.msg_iov = &iov;
+      msg.msg_iovlen = 1;
+      msg.msg_flags = 0;
+
+      cmsg.cmsg_level = SOL_SOCKET;
+      cmsg.cmsg_type = SCM_RIGHTS;
+      cmsg.cmsg_len = CMSG_LEN(int.sizeof);
+
+      int fd = -1;
+      memcpy(CMSG_DATA(&cmsg), cast(void*)&fd, int.sizeof);
+
+      auto received = recvmsg(this.handle() /* access to sock. */, &msg, 0);
+      memcpy(cast(void*)&fd, CMSG_DATA(&cmsg), int.sizeof);
+      return received;
+    }
+
+    override ptrdiff_t receive(void[] buf, SocketFlags flags)
+    {
+      return _receive(buf, flags);
+    }
+
+    override ptrdiff_t receive(void[] buf)
+    {
+      return receive(buf, SocketFlags.NONE);
+    }
+  }
+
+  UnixSocket[2] socketPair()
+  {
+    int[2] socks;
+    if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socks) == -1) {
+      throw new SocketOSException("Unable to create socket pair");
+    }
+
+    UnixSocket toUnixSocket(size_t id)
+    {
+      return new UnixSocket(cast(socket_t)socks[id], AddressFamily.UNIX);
+    }
+
+    return [toUnixSocket(0), toUnixSocket(1)];
+  }
+
+  unittest
+  {
+    import std.stdio;
+
+    immutable ubyte[] data = [1, 2, 3, 4];
+    auto pair = socketPair();
+    scope(exit) foreach (s; pair) s.close();
+
+    pair[0].send(data);
+
+    auto buf = new ubyte[data.length];
+    pair[1].receive(buf);
+
+    assert(buf == data);
+  }
+}
+else {
+  static assert(false);  // currently not supported other platform.
+}
