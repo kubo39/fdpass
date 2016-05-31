4fd07052e09eab1061a3526fd527acb2e8ad77e3
Hiroki Noda <kubo39@gmail.com>
2016-06-01 05:47:40 +0900

    Minor update.

--- source/fdpass.d
+++ source/fdpass.d
@@ -1,133 +1,133 @@
 module fdpass;
 
-version(linux)
+version(linux):
+
+import core.sys.posix.sys.socket;
+import core.stdc.string : memcpy;
+
+import std.socket;
+
+
+/**
+   Unix-domain socket.
+ */
+class UnixSocket : Socket
 {
-    private import core.sys.posix.sys.socket;
-    private import core.stdc.string : memcpy;
-
-    import std.socket : Socket,
-        socket_t,
-        SocketFlags,
-        AddressFamily,
-        SocketType,
-        ProtocolType,
-        SocketOSException;
-
-    class UnixSocket : Socket
+    // Constructor.
+    this(SocketType socketType)
     {
-        this(SocketType socketType)
-        {
-            super(AddressFamily.UNIX, socketType, ProtocolType.IP);
-        }
-
-        this(socket_t sock)
-        {
-            super(sock, AddressFamily.UNIX);
-        }
-
-        ptrdiff_t _send(const(void)[] buf, SocketFlags flags) @trusted
-        {
-            msghdr msg;
-            iovec iov;
-            cmsghdr cmsg;
-
-            iov.iov_base = cast(void*) buf;
-            iov.iov_len = buf.length;
-
-            msg.msg_name = null;
-            msg.msg_namelen = 0;
-            msg.msg_control = &cmsg;
-            msg.msg_controllen = cast(socklen_t) CMSG_LEN(int.sizeof);
-            msg.msg_iov = &iov;
-            msg.msg_iovlen = 1;
-            msg.msg_flags = 0;
-
-            cmsg.cmsg_level = SOL_SOCKET;
-            cmsg.cmsg_type = SCM_RIGHTS;
-            cmsg.cmsg_len = CMSG_LEN(int.sizeof);
-
-            socket_t fd = this.handle(); /* access to sock. */
-            memcpy(CMSG_DATA(&cmsg), cast(void*) &fd, int.sizeof);
-
-            return sendmsg(fd, &msg, flags);
-        }
-
-        override ptrdiff_t send(const(void)[] buf, SocketFlags flags)
-        {
-            return _send(buf, flags);
-        }
-
-        override ptrdiff_t send(const(void)[] buf)
-        {
-            return send(buf, SocketFlags.NONE);
-        }
-
-        ptrdiff_t _receive(void[] buf, SocketFlags flags) @trusted
-        {
-            msghdr msg;
-            iovec iov;
-            cmsghdr cmsg;
-
-            iov.iov_base = cast(void*) buf;
-            iov.iov_len = buf.length;
-
-            msg.msg_name = null;
-            msg.msg_namelen = 0;
-            msg.msg_control = &cmsg;
-            msg.msg_controllen = cast(socklen_t) CMSG_LEN(int.sizeof);
-            msg.msg_iov = &iov;
-            msg.msg_iovlen = 1;
-            msg.msg_flags = 0;
-
-            cmsg.cmsg_level = SOL_SOCKET;
-            cmsg.cmsg_type = SCM_RIGHTS;
-            cmsg.cmsg_len = CMSG_LEN(int.sizeof);
-
-            int fd = -1;
-            memcpy(CMSG_DATA(&cmsg), cast(void*) &fd, int.sizeof);
-
-            return recvmsg(this.handle() /* access to sock. */, &msg, flags);
-        }
-
-        override ptrdiff_t receive(void[] buf, SocketFlags flags)
-        {
-            return _receive(buf, flags);
-        }
-
-        override ptrdiff_t receive(void[] buf)
-        {
-            return receive(buf, SocketFlags.NONE);
-        }
+        super(AddressFamily.UNIX, socketType, ProtocolType.IP);
     }
 
-    UnixSocket[2] socketPair()
+    // ditto.
+    this(socket_t sock)
     {
-        int[2] socks;
-        if (socketpair(AF_UNIX, SOCK_STREAM, 0, socks) == -1)
-        {
-            throw new SocketOSException("Unable to create socket pair");
-        }
-
-        UnixSocket toUnixSocket(size_t id)
-        {
-            return new UnixSocket(cast(socket_t) socks[id]);
-        }
-
-        return [toUnixSocket(0), toUnixSocket(1)];
+        super(sock, AddressFamily.UNIX);
     }
 
-    unittest
+    ptrdiff_t _send(const(void)[] buf, SocketFlags flags) @trusted
     {
-        immutable ubyte[] data = [1, 2, 3, 4];
-        UnixSocket[2] pair = socketPair();
-        scope (exit) foreach (s; pair) s.close();
+        msghdr msg;
+        iovec iov;
+        cmsghdr cmsg;
 
-        pair[0].send(data);
+        iov.iov_base = cast(void*) buf;
+        iov.iov_len = buf.length;
 
-        auto buf = new ubyte[data.length];
-        pair[1].receive(buf);
+        msg.msg_name = null;
+        msg.msg_namelen = 0;
+        msg.msg_control = &cmsg;
+        msg.msg_controllen = cast(socklen_t) CMSG_LEN(int.sizeof);
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        msg.msg_flags = 0;
 
-        assert(buf == data);
+        cmsg.cmsg_level = SOL_SOCKET;
+        cmsg.cmsg_type = SCM_RIGHTS;
+        cmsg.cmsg_len = CMSG_LEN(int.sizeof);
+
+        socket_t fd = this.handle(); /* access to sock. */
+        memcpy(CMSG_DATA(&cmsg), cast(void*) &fd, int.sizeof);
+
+        return sendmsg(fd, &msg, flags);
+    }
+
+    override ptrdiff_t send(const(void)[] buf, SocketFlags flags)
+    {
+        return _send(buf, flags);
+    }
+
+    override ptrdiff_t send(const(void)[] buf)
+    {
+        return send(buf, SocketFlags.NONE);
+    }
+
+    ptrdiff_t _receive(void[] buf, SocketFlags flags) @trusted
+    {
+        msghdr msg;
+        iovec iov;
+        cmsghdr cmsg;
+
+        iov.iov_base = cast(void*) buf;
+        iov.iov_len = buf.length;
+
+        msg.msg_name = null;
+        msg.msg_namelen = 0;
+        msg.msg_control = &cmsg;
+        msg.msg_controllen = cast(socklen_t) CMSG_LEN(int.sizeof);
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        msg.msg_flags = 0;
+
+        cmsg.cmsg_level = SOL_SOCKET;
+        cmsg.cmsg_type = SCM_RIGHTS;
+        cmsg.cmsg_len = CMSG_LEN(int.sizeof);
+
+        int fd = -1;
+        memcpy(CMSG_DATA(&cmsg), cast(void*) &fd, int.sizeof);
+
+        return recvmsg(this.handle() /* access to sock. */, &msg, flags);
+    }
+
+    override ptrdiff_t receive(void[] buf, SocketFlags flags)
+    {
+        return _receive(buf, flags);
+    }
+
+    override ptrdiff_t receive(void[] buf)
+    {
+        return receive(buf, SocketFlags.NONE);
     }
 }
-else static assert(false, "Not supported platform.");
+
+
+UnixSocket[2] socketPair()
+{
+    int[2] socks;
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, socks) == -1)
+    {
+        throw new SocketOSException("Unable to create socket pair");
+    }
+
+    UnixSocket toUnixSocket(size_t id)
+    {
+        return new UnixSocket(cast(socket_t) socks[id]);
+    }
+
+    return [toUnixSocket(0), toUnixSocket(1)];
+}
+
+
+unittest
+{
+    immutable ubyte[] data = [1, 2, 3, 4];
+    UnixSocket[2] pair = socketPair();
+    scope (exit) foreach (s; pair) s.close();
+
+    pair[0].send(data);
+
+    auto buf = new ubyte[data.length];
+    pair[1].receive(buf);
+
+    assert(buf == data);
+}
